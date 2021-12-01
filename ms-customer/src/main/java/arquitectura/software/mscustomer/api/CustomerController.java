9c1e1040f67a11b5034ca8ca56d90a2422640001
Jhoan Porcel Peña <porcel.pena.jhoan@gmail.com>
2021-11-30 23:41:34 -0400

    ultimos cambios

--- ms-customer/src/main/java/arquitectura/software/mscustomer/api/CustomerController.java
+++ ms-customer/src/main/java/arquitectura/software/mscustomer/api/CustomerController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/api/customer")
-
+@CrossOrigin(origins="http://localhost:4200")
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
